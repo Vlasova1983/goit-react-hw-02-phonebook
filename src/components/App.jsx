d74a87c5bfb6bb982e28d3f27bf4273de4c8a141
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-27 18:36:54 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import {Component} from "react";
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App  extends Component {
+  state = {
+    contacts: [
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    name: '', 
+    filter: '',    
+    number: ''  
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state);
+    this.setState({ name: ''});
+  };
+  render(){
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmit}/>
+
+        <h2>Contacts</h2>
+        <Filter/>
+        <ContactList options={this.state.contacts}/>
+      </div>
+    );
+  }   
 };
