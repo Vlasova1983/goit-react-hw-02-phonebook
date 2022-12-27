e41842609787176720660c2ca1466dcfe6b16bfe
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-27 18:36:56 +0200

    Create ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,37 @@
+//  import { PropTypes } from 'prop-types';
+
+export const  ContactForm = ({onSubmit})=> {  
+  return (
+    <form className="" onSubmit={onSubmit}>
+      <div className="">
+        <label htmlFor="name" className="">
+          <p>Name</p>
+        </label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className="">
+        <label htmlFor="name" className="">
+          <p>Number</p>
+        </label>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>               
+
+      <button className="" type="submit">
+        Add contact
+      </button>
+    </form>
+      
+  );
+}
