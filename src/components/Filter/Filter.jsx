ca69dc7b716f7892cc3d4c1fa3049786cf5444c2
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-29 14:51:23 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,25 @@
 // import { PropTypes } from 'prop-types';
-export const Filter = () => {
+import styles  from '../Filter/Filter.module.css';
+
+
+export const Filter = ({onFilter}) => {
+
+    const handleFilter = event =>{
+        const nameFilter = event.target.value.toLowerCase()
+        console.log(nameFilter);
+        onFilter(nameFilter);
+    };
+
     return (
-        <div className="">
-            <label htmlFor="name" className="">
+        <div className={styles.filter}>
+            <label htmlFor="filter" className="">
                 <p>Find contacts by name</p>
             </label>
             <input
+                id="filter"
                 type="text"
                 name="filter"
+                onChange={handleFilter}               
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
